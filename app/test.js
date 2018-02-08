5613cec49086c5a9047e95caa12bff75ecfd4573
Steve Kinney <hello@stevekinney.net>
2018-02-08 13:42:58 -0700

    WIP Better setup for Spectron

--- app/test.js
+++ app/test.js
@@ -1,24 +1,12 @@
 const Application = require('spectron').Application;
 const path = require('path');
 
-let electronPath = path.join(
-  __dirname,
-  '..',
-  'node_modules',
-  '.bin',
-  'electron',
-);
-
-if (process.platform === 'win32') {
-  electronPath += '.cmd';
-}
-
 describe('application launch', () => {
   let app;
 
   beforeEach(() => {
     app = new Application({
-      path: electronPath,
+      path: require('electron'),
       args: [path.join(__dirname, './main.js')],
     });
     return app.start();
